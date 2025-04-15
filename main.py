03b428d9049779a5f43dd3713ad474778c76e526
Luiz Miguel <luizmiguelmds2004@gmail.com>
2025-04-15 18:32:46 -0300

    função do gerador ramdomico de questões

--- main.py
+++ main.py
@@ -8,13 +8,31 @@ app = FastAPI()
 # Liberação do Cors para o front
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
-    allow_credentials=true,
+    allow_origins=["*"],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
+# Leitura da planilha
 df = pd.read_excel("Banco de Questões - Geografia.xlsx")
 
-df = df[df["Enuciado"],notna() & df["Resposta correta"].notna()]
+#tratamento para remover linhas vazias e incompletas
+df = df[df["Enuciado"].notna() & df["Resposta correta"].notna()]
 
+@app.get("/questao")
+def get_questao():
+    questao = df.sample(1).iloc[0]  # Pega uma questão aleatória
+    return {
+        "id": questao["ID Questão"],
+        "enuciado": questao["Enuciado"],
+        "alternativas": {
+            "A": questao["A"],
+            "B": questao["B"],
+            "C": questao["C"],
+            "D": questao["D"],
+            "E": questao["E"]
+        },
+        "resposta_correta": questao["Resposta correta"],
+        "resposta_texto": questao["Texto da resposta correta"]
+    }
